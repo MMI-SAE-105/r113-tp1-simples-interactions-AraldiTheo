5a03f8f31ee8881ad09cba3b6ceff4a59c70022b
Araldi Théo <theo.araldi@edu.univ-fcomte.fr>
2024-11-08 10:59:44 +0100

    ajout du code dans le JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,15 @@
 // TODO
+const aMettreEnRouge = document.querySelector("#a-mettre-en-rouge");
+const enRougeSuiteAClick = document.querySelector("#en-rouge-a-click");
+const collH2 = document.querySelectorAll("h2");
+
+aMettreEnRouge.style.color = "red";
+
+enRougeSuiteAClick.addEventListener(click, (evt)=>{
+    enRougeSuiteAClick.style.color = "red"
+});
+collH2.forEach((elm)=>{
+    elm.addEventListener("click", (evt)=>{
+        evt.target.style.color = "red";
+    });
+});
